f1791afffda00aefbc81d7ba3ddf85f7fcd886d4
Gitlab CI Network <network@sportradar.com>
2023-08-08 13:51:12 +0200

    shared 0.4.11 release

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://artifacts.mdp.sportradar.com/apple/native-widgets-core/a1edcf74/release/shared-iOS-0.4.11.zip",
-            checksum: "1a712524fb0157e269bf8646b1d74ff7768bcdc8edcdc4f9db67e4b2d0b757ca"
+            url: "https://artifacts.mdp.sportradar.com/apple/native-widgets-core/336e31a6/release/shared-iOS-0.4.11.zip",
+            checksum: "7eb0471fc9c3afab8d54d71d1fbecfd284d0e5a757b17688edff67c90dbe8614"
         ),
     ]
 )
